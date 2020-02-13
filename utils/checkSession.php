2a14940e8765e9dafd92edb21a145de579f9a8b5
ariesmacandili <macandili.aries@gmail.com>
2020-02-14 00:36:38 +0800

    Updated sql file and added libraries for session validations.

---
+++ utils/checkSession.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include_once 'utils/libSession.php';
+
+/**
+ * This file is automatically prepended to any file via .htaccess file.
+ * This also acts as a middleware that checks if a user is currently logged-in or not.
+ * It also performs redirection. 
+ */
+
+if (Session::isset('isLoggedIn') === true) {
+    if (preg_match('/\/homepage\/|\/login\//i', $_SERVER['REQUEST_URI'])) {
+        // Redirect to dashboard.
+        header('Location: /Nexus/dashboard');
+        exit();
+    }
+} else {
+    if (!preg_match('/\/homepage\/|\/login\//i', $_SERVER['REQUEST_URI'])) {
+        // Redirect to homepage.
+        header('Location: /Nexus/homepage/welcome');
+        exit();
+    }
+}
