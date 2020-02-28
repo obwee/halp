115e15662fea10f76ca71ecc1e9b76a2cb39939a
ariesmacandili <macandili.aries@gmail.com>
2020-02-29 01:06:11 +0800

    Quotation saving done.

--- utils/nexus.sql
+++ utils/nexus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2020 at 03:12 PM
+-- Generation Time: Feb 28, 2020 at 06:04 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -42,9 +42,11 @@ CREATE TABLE `tbl_courses` (
 -- Dumping data for table `tbl_courses`
 --
 
-INSERT INTO `tbl_courses` VALUES(1, 'CCNA v4', '12000', 'Implementing and Administering Cisco Solutions', '200-301');
-INSERT INTO `tbl_courses` VALUES(2, 'CCNP', '12000', 'Implementing Cisco Enterprise Advanced Routing and Services', '');
-INSERT INTO `tbl_courses` VALUES(3, 'MCP', '8000', 'Windows 2012 R2', '20410');
+INSERT INTO `tbl_courses` (`id`, `courseName`, `coursePrice`, `courseDescription`, `examCode`) VALUES
+(1, 'CCNA v4', '12000', 'Implementing and Administering Cisco Solutions', '200-301'),
+(2, 'CCNP', '12000', 'Implementing Cisco Enterprise Advanced Routing and Services', ''),
+(3, 'MCP', '8000', 'Windows 2012 R2', '20410'),
+(4, 'Ethical Hacking with Penetration Testing', '3000', '', '');
 
 -- --------------------------------------------------------
 
@@ -63,6 +65,17 @@ CREATE TABLE `tbl_emails` (
   `dateSent` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tbl_emails`
+--
+
+INSERT INTO `tbl_emails` (`id`, `firstName`, `middleName`, `lastName`, `email`, `title`, `message`, `dateSent`) VALUES
+(29, 'asfmalkf', 'fjskljfkl', 'fjskalfjsakl', 'aubrey@gmail.com', 'sfajsfkl', 'fjkslfja<br />\r\ns', '2020-02-21 23:26:32'),
+(30, 'asfmalkf', 'fjskljfkl', 'fjskalfjsakl', 'aubrey@gmail.com', 'sfajsfkl', 'fjkslfja<br />\r\ns', '2020-02-21 23:26:37'),
+(31, 'asfmalkf', 'fjskljfkl', 'fjskalfjsakl', 'aubrey@gmail.com', 'sfajsfkl', 'fjkslfja<br />\r\ns', '2020-02-21 23:32:09'),
+(32, 'asfmalkf', 'fjskljfkl', 'fjskalfjsakl', 'aubrey@gmail.com', 'sfajsfkl', 'fjkslfja<br />\r\ns', '2020-02-21 23:32:54'),
+(33, 'fsafkajk', 'jfkslajfkalsj', 'kfjsakfjaskl', 'jfsklasjfkl@gmail.com', 'fjsakfjsak', 'jfklsajfklasjfklsafj<br />\r\n<br />\r\n2141421412', '2020-02-21 23:37:23');
+
 -- --------------------------------------------------------
 
 --
@@ -73,17 +86,6 @@ CREATE TABLE `tbl_inclusions` (
   `id` int(11) NOT NULL,
   `courseId` int(11) NOT NULL,
   `inclusion` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_paymentmethods`
---
-
-CREATE TABLE `tbl_paymentmethods` (
-  `id` int(11) NOT NULL,
-  `methodName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -104,19 +106,45 @@ CREATE TABLE `tbl_payments` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_quotations`
+-- Table structure for table `tbl_payment_methods`
 --
 
-CREATE TABLE `tbl_quotations` (
+CREATE TABLE `tbl_payment_methods` (
   `id` int(11) NOT NULL,
+  `methodName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_quotation_details`
+--
+
+CREATE TABLE `tbl_quotation_details` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `senderId` int(11) NOT NULL,
+  `courseId` varchar(255) NOT NULL,
+  `scheduleId` int(11) NOT NULL,
+  `dateSent` datetime NOT NULL,
+  `isCompanySponsored` tinyint(1) NOT NULL DEFAULT 0,
+  `isQuotationSent` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_quotation_senders`
+--
+
+CREATE TABLE `tbl_quotation_senders` (
+  `quoteSenderId` int(11) NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `middleName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `courseId` varchar(255) NOT NULL,
-  `scheduleId` int(11) NOT NULL,
-  `dateSent` datetime NOT NULL,
-  `isQuotationSent` tinyint(1) NOT NULL DEFAULT 0
+  `contactNum` varchar(255) NOT NULL,
+  `companyName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -130,9 +158,20 @@ CREATE TABLE `tbl_schedules` (
   `courseId` int(11) NOT NULL,
   `venueId` int(11) NOT NULL,
   `scheduleType` varchar(255) NOT NULL,
-  `fromDate` datetime NOT NULL,
-  `toDate` datetime NOT NULL
+  `fromDate` date NOT NULL,
+  `toDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_schedules`
+--
+
+INSERT INTO `tbl_schedules` (`id`, `courseId`, `venueId`, `scheduleType`, `fromDate`, `toDate`) VALUES
+(2, 1, 1, 'Weekdays', '2020-03-16', '2020-03-20'),
+(3, 2, 1, 'Weekdays', '2020-03-23', '2020-03-27'),
+(4, 3, 2, 'Weekends', '2020-03-28', '2020-03-29'),
+(5, 4, 1, 'Sundays', '2020-03-29', '2020-03-29'),
+(6, 1, 1, 'Weekdays', '2020-03-09', '2020-03-13');
 
 -- --------------------------------------------------------
 
@@ -151,6 +190,14 @@ CREATE TABLE `tbl_training` (
   `certificateIssued` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tbl_training`
+--
+
+INSERT INTO `tbl_training` (`id`, `studentId`, `courseId`, `instructorId`, `scheduleId`, `isDone`, `paymentId`, `certificateIssued`) VALUES
+(1, 3, 1, 2, 1, 0, 1, 0),
+(2, 4, 1, 2, 1, 0, 2, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -158,7 +205,7 @@ CREATE TABLE `tbl_training` (
 --
 
 CREATE TABLE `tbl_users` (
-  `userID` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `firstName` varchar(255) NOT NULL,
@@ -175,7 +222,18 @@ CREATE TABLE `tbl_users` (
 -- Dumping data for table `tbl_users`
 --
 
-INSERT INTO `tbl_users` VALUES(1, 'chris', 'chris123', 'Christopher', 'Iglesia', 'Buenaventura', 'Super Admin', '', '09261759759', 'angelikaaubreyarbiol@gmail.com', 'Active');
+INSERT INTO `tbl_users` (`userId`, `username`, `password`, `firstName`, `middleName`, `lastName`, `position`, `companyName`, `contactNum`, `email`, `status`) VALUES
+(1, 'chris', 'chris123', 'Christopher', 'Iglesia', 'Buenaventura', 'Super Admin', '', '09261759759', 'angelikaaubreyarbiol@gmail.com', 'Active'),
+(7, 'markus', '123456789', 'Mark Exequiel', 'Reambillo', 'Sale', 'Student', 'Concentrix', '516874621345', 'markexequielsale@gmail.com', 'Active'),
+(59, 'student17', 'student', 'Leonard Mayer', 'White', 'Daniel', 'Student', 'Mayer, Fisher and Dibbert', '14531', 'wehner.twila@example.net', 'Inactive'),
+(64, 'student10', 'student', 'Loyal Wisozk', 'Gutkowski', 'Reinger', 'Student', 'Dare-Bernhard', '88381', 'ashleigh68@example.org', 'Active'),
+(70, 'student14', 'student', 'Bethel Bergstrom', 'Lakin', 'Roob', 'Student', 'Greenfelder, Olson and DuBuque', '32110', 'gjacobs@example.net', 'Active'),
+(73, 'student16', 'student', 'Haskell Schmeler', 'Harvey', 'Blick', 'Student', 'Lynch Group', '80359', 'jason.reichert@example.com', 'Active'),
+(79, 'student11', 'student', 'Dave Breitenberg V', 'Spencer', 'Weber', 'Student', 'Schiller Ltd', '58602', 'lonie77@example.com', 'Inactive'),
+(92, 'student15', 'student', 'Ora Mayert Sr.', 'Pouros', 'Heathcote', 'Student', 'Eichmann Group', '16384', 'tschowalter@example.com', 'Active'),
+(97, 'student12', 'student', 'Fay Gorczany', 'Bergnaum', 'Hilpert', 'Student', 'Botsford and Sons', '89606', 'coreilly@example.net', 'Inactive'),
+(98, 'aries', 'student', 'Aries', 'Valenzuela', 'Macandili', 'Student', 'Cafe24 PH', '09161225985', 'macandili.aries@gmail.com', 'Active'),
+(103, 'student13', 'student', 'Milton Dibbert', 'Quitzon', 'Hyatt', 'Student', 'Bradtke-West', '28141', 'gregg33@example.net', 'Inactive');
 
 -- --------------------------------------------------------
 
@@ -185,8 +243,17 @@ INSERT INTO `tbl_users` VALUES(1, 'chris', 'chris123', 'Christopher', 'Iglesia',
 
 CREATE TABLE `tbl_venue` (
   `id` int(11) NOT NULL,
-  `venue` varchar(255) NOT NULL
+  `venue` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_venue`
+--
+
+INSERT INTO `tbl_venue` (`id`, `venue`, `address`) VALUES
+(1, 'Manila', 'Room 401 Dona Amparo Bldg., Espana Blvd., Manila'),
+(2, 'Makati', 'Unit 2417, 24th Floor Cityland 10 Tower 2, 154 H.V. Dela Costa St., Ayala North, Makati City\r\n');
 
 --
 -- Indexes for dumped tables
@@ -211,22 +278,28 @@ ALTER TABLE `tbl_inclusions`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tbl_paymentmethods`
---
-ALTER TABLE `tbl_paymentmethods`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `tbl_payments`
 --
 ALTER TABLE `tbl_payments`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tbl_quotations`
+-- Indexes for table `tbl_payment_methods`
 --
-ALTER TABLE `tbl_quotations`
+ALTER TABLE `tbl_payment_methods`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_quotation_details`
+--
+ALTER TABLE `tbl_quotation_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_quotation_senders`
+--
+ALTER TABLE `tbl_quotation_senders`
+  ADD PRIMARY KEY (`quoteSenderId`);
 
 --
 -- Indexes for table `tbl_schedules`
@@ -244,7 +317,7 @@ ALTER TABLE `tbl_training`
 -- Indexes for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  ADD PRIMARY KEY (`userID`);
+  ADD PRIMARY KEY (`userId`);
 
 --
 -- Indexes for table `tbl_venue`
@@ -260,24 +333,18 @@ ALTER TABLE `tbl_venue`
 -- AUTO_INCREMENT for table `tbl_courses`
 --
 ALTER TABLE `tbl_courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_emails`
 --
 ALTER TABLE `tbl_emails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `tbl_inclusions`
 --
 ALTER TABLE `tbl_inclusions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tbl_paymentmethods`
---
-ALTER TABLE `tbl_paymentmethods`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -287,34 +354,46 @@ ALTER TABLE `tbl_payments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `tbl_quotations`
+-- AUTO_INCREMENT for table `tbl_payment_methods`
 --
-ALTER TABLE `tbl_quotations`
+ALTER TABLE `tbl_payment_methods`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_quotation_details`
+--
+ALTER TABLE `tbl_quotation_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `tbl_quotation_senders`
+--
+ALTER TABLE `tbl_quotation_senders`
+  MODIFY `quoteSenderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `tbl_schedules`
 --
 ALTER TABLE `tbl_schedules`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_training`
 --
 ALTER TABLE `tbl_training`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
 
 --
 -- AUTO_INCREMENT for table `tbl_venue`
 --
 ALTER TABLE `tbl_venue`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
