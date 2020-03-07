1d0e720552fdac307bbf2b06b0bfc4301ecc373d
ariesmacandili <macandili.aries@gmail.com>
2020-03-07 21:55:45 +0800

    Fixed bill to company bug.

--- dashboard/js/dashboard.sentQuotations.js
+++ dashboard/js/dashboard.sentQuotations.js
@@ -1,7 +1,163 @@
 var oSentQuotations = (() => {
 
+    let oTblSenders = $('#quotationSenders');
+    let oTblRequests = $('#quotationRequests');
+    let oTblDetails = $('#quotationDetails');
+    
+    let oColumns = {
+        aSender: [
+            {
+                title: 'Sender Name', render: (aData, oType, oRow) =>
+                    [oRow.firstName, oRow.middleName, oRow.lastName].join(' ')
+            },
+            {
+                title: 'Email Address', data: 'email'
+            },
+            {
+                title: 'Contact Number', data: 'contactNum'
+            },
+            {
+                title: 'Actions', className: 'text-center', render: (aData, oType, oRow) =>
+                    `<button class="btn btn-primary btn-sm" data-toggle="modal" id="viewRequest" data-sender-id="${oRow.senderId}" data-user-id="${oRow.userId}">
+                        <i class="fa fa-eye"></i>
+                    </button>
+                    <button class="btn btn-danger btn-sm" data-toggle="modal" id="deleteSender" data-sender-id="${oRow.senderId}" data-user-id="${oRow.userId}">
+                        <i class="fa fa-trash"></i>
+                    </button>`
+            },
+        ],
+        aRequest: [
+            {
+                title: 'Date Requested', className: 'text-center', data: 'fullDate'
+            },
+            {
+                title: 'Number of Courses', className: 'text-center', data: 'numberOfCourses'
+            },
+            {
+                title: 'Company Name', className: 'text-center', render: (oData, oType, oRow) =>
+                    (oRow.companyName === '') ? '-' : oRow.companyName
+            },
+            {
+                title: 'Bill to Company', className: 'text-center', render: (oData, oType, oRow) =>
+                    (oRow.companyName === '') ? '-' : oRow.isCompanySponsored
+            },
+            {
+                title: 'Actions', className: 'text-center', render: (aData, oType, oRow) =>
+                    `<button class="btn btn-primary btn-sm viewDetails" data-toggle="modal" data-sender-id="${oRow.senderId}" data-user-id="${oRow.userId}" data-date-requested="${oRow.dateRequested}">
+                        <i class="fa fa-eye"></i>
+                    </button>
+                    <button class="btn btn-success btn-sm" data-toggle="modal" id="approveRequest" data-sender-id="${oRow.senderId}" data-user-id="${oRow.userId}" data-date-requested="${oRow.dateRequested}">
+                        <i class="fa fa-check"></i>
+                    </button>
+                    <button class="btn btn-warning btn-sm" data-toggle="modal" id="editRequest" data-sender-id="${oRow.senderId}" data-user-id="${oRow.userId}" data-date-requested="${oRow.dateRequested}">
+                        <i class="fa fa-pencil-alt"></i>
+                    </button>
+                    <button class="btn btn-danger btn-sm" data-toggle="modal" id="deleteRequest" data-sender-id="${oRow.senderId}" data-user-id="${oRow.userId}" data-date-requested="${oRow.dateRequested}">
+                        <i class="fa fa-trash"></i>
+                    </button>`
+            },
+        ],
+        aDetails: [
+            {
+                title: 'Course Name', className: 'text-center', render: (aData, oType, oRow) =>
+                    (oRow.courseName === '') ? ' - ' : oRow.courseName
+            },
+            {
+                title: 'Course Description', className: 'text-center', render: (aData, oType, oRow) =>
+                    (oRow.courseDescription === '') ? ' - ' : oRow.courseDescription
+            },
+            {
+                title: 'Exam Code', className: 'text-center', render: (aData, oType, oRow) =>
+                    (oRow.examCode === '') ? ' - ' : oRow.examCode
+            },
+            {
+                title: 'Training Date', className: 'text-center', render: (aData, oType, oRow) =>
+                    [oRow.fromDate, oRow.toDate].join(' - ')
+            },
+            {
+                title: 'Number of Persons', className: 'text-center', data: 'numPax'
+            }
+        ]
+    };
+
     function init() {
-        $('#tbl_quotations').DataTable();
+        populateSendersTable();
+        // fetchCoursesAndSchedules();
+        // setEvents();
+    }
+
+    function populateSendersTable() {
+        let oAjax = {
+            url: `../utils/ajax.php?class=Quotations&action=fetchSenders`,
+            type: 'POST',
+            data: { iIsQuotationSent: 1 },
+            dataType: 'JSON',
+            dataSrc: function (oJson) {
+                aSenders = oJson.aData;
+                return aSenders;
+            },
+            async: false
+        };
+
+        let aColumnDefs = [
+            { orderable: false, targets: [1, 2, 3] }
+        ];
+
+        loadTable(oTblSenders.attr('id'), oAjax, oColumns.aSender, aColumnDefs);
+    }
+
+    function populateRequestsTable(oData) {
+        let oAjax = {
+            url: `../utils/ajax.php?class=Quotations&action=fetchRequests`,
+            type: 'POST',
+            data: oData,
+            dataSrc: (oJson) => {
+                return oJson;
+            },
+            async: false
+        };
+
+        let aColumnDefs = [
+            { orderable: false, targets: [1, 2, 3, 4] }
+        ];
+
+        loadTable(oTblRequests.attr('id'), oAjax, oColumns.aRequest, aColumnDefs);
+    }
+
+    function populateDetailsTable(oData) {
+        let oAjax = {
+            url: `../utils/ajax.php?class=Quotations&action=fetchDetails`,
+            type: 'POST',
+            data: oData,
+            dataSrc: (oJson) => {
+                return oJson;
+            },
+            async: false
+        };
+
+        let aColumnDefs = [
+            { orderable: false, targets: '_all' }
+        ];
+
+        loadTable(oTblDetails.attr('id'), oAjax, oColumns.aDetails, aColumnDefs);
+    }
+
+    function loadTable(sTableName, oData, aColumns, aColumnDefs) {
+        $(`#${sTableName} > tbody`).empty().parent().DataTable({
+            destroy: true,
+            deferRender: true,
+            ajax: oData,
+            responsive: true,
+            pagingType: 'first_last_numbers',
+            pageLength: 4,
+            ordering: true,
+            searching: true,
+            lengthChange: true,
+            lengthMenu: [[4, 8, 12, 16, 20, 24, -1], [4, 8, 12, 16, 20, 24, 'All']],
+            info: true,
+            columns: aColumns,
+            columnDefs: aColumnDefs
+        });
     }
 
     return {
