67e9d288b0ccdd4bb0e785427a26416d1a5a6526
Angelika Aubrey Arbiol <angelikaaubreyarbiol@gmail.com>
2020-04-05 20:30:50 +0800

    ..

--- template/studentHeader.php
+++ template/studentHeader.php
@@ -109,9 +109,9 @@
                             </span>
                         </a>
                         <ul class="treeview-menu">
-                            <li><a href="studendUnpaid.php"><i class="fas fa-times"></i>&emsp; Unpaid Reservations</a></li>
-                            <li><a href="studentPartialPayment.php"><i class="fas fa-check"></i>&emsp;Partially Paid Reservations</a></li>
-                            <li><a href="studentFullPayment.php"><i class="fas fa-check-double"></i>&emsp;Fully Paid Reservations</a></li>
+                            <li><a href="stud.Unpaid.php"><i class="fas fa-times"></i>&emsp; Unpaid Reservations</a></li>
+                            <li><a href="partialPayment.php"><i class="fas fa-check"></i>&emsp;Partially Paid Reservations</a></li>
+                            <li><a href="fullPayment.php"><i class="fas fa-check-double"></i>&emsp;Fully Paid Reservations</a></li>
                         </ul>
                     </li>
                     <li>
